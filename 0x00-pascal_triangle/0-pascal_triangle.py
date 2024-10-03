41023c3694385413554e0f24de709e0dc318a6dd
Your Name <you@example.com>
2024-10-03 17:05:47 +0800

    fixing issues

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
-a function that prints pascal triangle
+0. Pascal's Triangle
 """
-
-
-from math import comb
 
 
 def pascal_triangle(n):
     """Create a function def pascal_triangle(n): that returns a list of lists
     of integers representing the Pascal’s triangle of n
     """
-    if n <= 0:
-        return []
-    else:
-        L = []
-        for i in range(n):
-            L.append([comb(i, j) for j in range(i + 1)])
-    return L
+    res = []
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            C = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                C = C * (i - j) // j
+            res.append(level)
+    return res
