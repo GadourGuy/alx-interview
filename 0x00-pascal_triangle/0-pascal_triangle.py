45dbbf3ad02a270b98376778a7224dc37ad2ab35
Your Name <you@example.com>
2024-10-03 17:10:45 +0800

    fixing issues

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
 """
-0. Pascal's Triangle
+a function that prints pascal triangle
 """
+
+
+def fact(n):
+    """ function to count factorial
+    """
+    if n == 0 or n == 1:
+        return 1
+    res = 1
+
+    for i in range(2, n+1):
+        res = res * i
+
+    return res
+
+
+def nCr(n, r):
+    """ function to count nCr"""
+    return (int(fact(n) / (fact(r) * fact(n - r))))
 
 
 def pascal_triangle(n):
     """Create a function def pascal_triangle(n): that returns a list of lists
     of integers representing the Pascal’s triangle of n
     """
-    res = []
-    if n > 0:
-        for i in range(1, n + 1):
-            level = []
-            C = 1
-            for j in range(1, i + 1):
-                level.append(C)
-                C = C * (i - j) // j
-            res.append(level)
-    return res
+    if n <= 0:
+        return []
+    else:
+        L = []
+        for i in range(n):
+            L.append([nCr(i, j) for j in range(i + 1)])
+    return L
